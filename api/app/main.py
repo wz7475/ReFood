53cbe756d44b7d88e86ad6404e4b365d123a9722
Rafał <szczepaniak.rafal@gmail.com>
2023-12-10 16:33:15 +0100

    feat: add DELETE endpoints and fix auto-increment

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 from .models import Base, Users, Addresses, Offers, Dishes
 from sqlalchemy.sql.functions import current_timestamp
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, load_only
 from sqlalchemy import create_engine
 
 
@@ -25,10 +25,18 @@ async def read_users():
     db.close()
     return users
 
-@app.post("/users")
-async def add_user(id : int = Body(), name: str = Body(), surname: str = Body(), age: int = Body(), address_id : int = Body(), phone_nr : str = Body(), rating: int = Body()):
+@app.get("/users/last_id")
+async def get_last_user_id():
     db = SessionLocal()
-    user = Users(id=id, name=name, surname=surname, age=age, address_id=address_id, phone_nr=phone_nr, rating=rating)
+    last_id = db.query(Users.id).all()
+    db.close()
+    return max([row[0] for row in last_id])
+
+@app.post("/users")
+async def add_user(name: str = Body(), surname: str = Body(), age: int = Body(), address_id : int = Body(), phone_nr : str = Body(), rating: int = Body()):
+    db = SessionLocal()
+    user_id = await get_last_user_id() + 1
+    user = Users(id=user_id, name=name, surname=surname, age=age, address_id=address_id, phone_nr=phone_nr, rating=rating)
     db.add(user)
     db.commit()
     db.close()
@@ -40,6 +48,16 @@ async def read_user_by_id(user_id: int):
     db.close()
     return user
 
+@app.delete("/users/{user_id}")
+async def delete_user(user_id: int):
+    db = SessionLocal()
+    user = db.query(Users).filter(Users.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.delete(user)
+    db.commit()
+    db.close()
+
 @app.get("/addresses")
 async def read_addresses():
     db = SessionLocal()
@@ -47,10 +65,18 @@ async def read_addresses():
     db.close()
     return addresses
 
-@app.post("/addresses")
-async def add_address(id : int = Body(), street_name: str = Body(), house_nr: int = Body(), apartament_nr: int = Body(), city : str = Body()):
+@app.get("/addresses/last_id")
+async def get_last_address_id():
     db = SessionLocal()
-    address = Addresses(id=id, street_name=street_name, house_nr=house_nr, apartament_nr=apartament_nr, city=city)
+    last_id = db.query(Addresses.id).all()
+    db.close()
+    return max([row[0] for row in last_id])
+
+@app.post("/addresses")
+async def add_address(street_name: str = Body(), house_nr: int = Body(), apartament_nr: int = Body(), city : str = Body()):
+    db = SessionLocal()
+    address_id = await get_last_address_id() + 1
+    address = Addresses(id=address_id, street_name=street_name, house_nr=house_nr, apartament_nr=apartament_nr, city=city)
     db.add(address)
     db.commit()
     db.close()
@@ -62,6 +88,16 @@ async def read_address_by_id(address_id: int):
     db.close()
     return address
 
+@app.delete("/addresses/{address_id}")
+async def delete_address(address_id: int):
+    db = SessionLocal()
+    address = db.query(Addresses).filter(Addresses.id == address_id).first()
+    if not address:
+        raise HTTPException(status_code=404, detail="Address not found")
+    db.delete(address)
+    db.commit()
+    db.close()
+
 @app.get("/offers")
 async def read_offers():
     db = SessionLocal()
@@ -69,10 +105,18 @@ async def read_offers():
     db.close()
     return offers
 
-@app.post("/offers")
-async def add_offer(id : int = Body(), dish_id: int = Body(), seller_id: int = Body(), address_id: int = Body()):
+@app.get("/offers/last_id")
+async def get_last_offer_id():
     db = SessionLocal()
-    offer = Offers(id=id, dish_id=dish_id, seller_id=seller_id, address_id=address_id, creation_date=current_timestamp())
+    last_id = db.query(Offers.id).all()
+    db.close()
+    return max([row[0] for row in last_id])
+
+@app.post("/offers")
+async def add_offer(dish_id: int = Body(), seller_id: int = Body(), address_id: int = Body()):
+    db = SessionLocal()
+    offer_id = await get_last_offer_id() + 1
+    offer = Offers(id=offer_id, dish_id=dish_id, seller_id=seller_id, address_id=address_id, creation_date=current_timestamp())
     db.add(offer)
     db.commit()
     db.close()
@@ -84,6 +128,16 @@ async def read_offer_by_id(offer_id: int):
     db.close()
     return offer
 
+@app.delete("/offers/{offer_id}")
+async def delete_offer(offer_id: int):
+    db = SessionLocal()
+    offer = db.query(Offers).filter(Offers.id == offer_id).first()
+    if not offer:
+        raise HTTPException(status_code=404, detail="Offer not found")
+    db.delete(offer)
+    db.commit()
+    db.close()
+
 @app.get("/dishes")
 async def read_dishes():
     db = SessionLocal()
@@ -91,17 +145,35 @@ async def read_dishes():
     db.close()
     return dishes
 
-@app.post("/dishes")
-async def add_dish(id : int = Body(), name : str = Body(), is_vegetarian: bool = Body(), description: str = Body(), price: int = Body(), how_many_days_before_expiration : float = Body()):
+@app.get("/dishes/last_id")
+async def get_last_dish_id():
     db = SessionLocal()
-    dish = Dishes(id=id, name=name, is_vegetarian=is_vegetarian, price=price, description=description, how_many_days_before_expiration=how_many_days_before_expiration)
+    last_id = db.query(Dishes.id).all()
+    db.close()
+    return max([row[0] for row in last_id])
+
+@app.post("/dishes")
+async def add_dish(name : str = Body(), is_vegetarian: bool = Body(), description: str = Body(), price: int = Body(), how_many_days_before_expiration : float = Body()):
+    db = SessionLocal()
+    dish_id = await get_last_dish_id() + 1
+    dish = Dishes(id=dish_id, name=name, is_vegetarian=is_vegetarian, price=price, description=description, how_many_days_before_expiration=how_many_days_before_expiration)
     db.add(dish)
     db.commit()
     db.close()
 
 @app.get("/dishes/{dish_id}")
-async def read_address_by_id(dish_id: int):
+async def read_dishes_by_id(dish_id: int):
     db = SessionLocal()
     dish = db.query(Dishes).filter(Dishes.id == dish_id).first()
     db.close()
     return dish
+
+@app.delete("/dishes/{dish_id}")
+async def delete_dish(dish_id: int):
+    db = SessionLocal()
+    dish = db.query(Dishes).filter(Dishes.id == dish_id).first()
+    if not dish:
+        raise HTTPException(status_code=404, detail="Dish not found")
+    db.delete(dish)
+    db.commit()
+    db.close()

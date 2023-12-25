fc30434b99375f0152c028eccd2cda8767434b78
gambolkf <gambolkf@gmail.com>
2023-12-25 22:29:09 +0100

    feat: implemented adding offers REF-27

--- api/app/main.py
+++ api/app/main.py
@@ -9,19 +9,22 @@ from .logger import get_logger
 from .models import Base, Users, Dishes, Offers, DishTags, OfferState, TagsValues, Tags
 from sqlalchemy.sql.functions import current_timestamp
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from elasticsearch import Elasticsearch
 from .cfg import SQLALCHEMY_DATABASE_URL, ADD_OFFER_QUEUE, DELETE_OFFER_QUEUE, OFFER_INDEX
 from .es_tools import get_by_fulltext
 from .connectors import get_rabbitmq_connection, get_es_connection
 from .sessions import SessionData, backend, cookie, verifier
 from uuid import UUID, uuid4
+from typing import List
 
 # SQLAlchemy configuration
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 connections = {}
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger = get_logger()
@@ -38,10 +41,10 @@ async def lifespan(app: FastAPI):
     yield
     connections.clear()
 
+
 app = FastAPI(lifespan=lifespan)
 
 origins = ["*"]
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -71,9 +74,10 @@ async def test_es_add_offer(offer_id: int):
         "description": f"test{offer_id} offer hdyż",
     }
     connections["add-channel"].basic_publish(exchange='',
-                            routing_key=ADD_OFFER_QUEUE,
-                            body=json.dumps(offer))
+                                             routing_key=ADD_OFFER_QUEUE,
+                                             body=json.dumps(offer))
     return offer
+
 
 @app.get("/test-es-delete-offer/{offer_id}")
 async def test_es_delete_offer(offer_id: int):
@@ -81,9 +85,10 @@ async def test_es_delete_offer(offer_id: int):
         "id": offer_id,
     }
     connections["delete-channel"].basic_publish(exchange='',
-                            routing_key=DELETE_OFFER_QUEUE,
-                            body=json.dumps(offer))
+                                                routing_key=DELETE_OFFER_QUEUE,
+                                                body=json.dumps(offer))
     return offer
+
 
 @app.get("/test-es-query/{pattern}")
 async def test_es_query(pattern: str):
@@ -94,22 +99,27 @@ async def test_es_query(pattern: str):
 
 def init_tag_table(db: SessionLocal):
     if len(db.query(Tags).all()) == 0:
-        for idx, tag_value in enumerate([TagsValues.VEGETARIAN, TagsValues.GLUTEN_FREE, TagsValues.SUGAR_FREE, TagsValues.SHOULD_BE_EATEN_WARM, TagsValues.SPICY]):
+        for idx, tag_value in enumerate(
+                [TagsValues.VEGETARIAN, TagsValues.GLUTEN_FREE, TagsValues.SUGAR_FREE, TagsValues.SHOULD_BE_EATEN_WARM,
+                 TagsValues.SPICY]):
             tag = Tags(id=idx, tag=tag_value)
             db.add(tag)
         db.commit()
 
 
-@app.get("/")
-async def read_root():
+@app.get("/", dependencies=[Depends(cookie)])
+async def read_root(session_data: SessionData = Depends(verifier)):
     return "ReFood"
 
 
 # Sesions
 @app.post("/login")
-async def create_session(response: Response, login: str = Body(), password: str = Body(), db: SessionLocal = Depends(get_db)):
-    if db.query(Users).filter_by(login=login).first() is not None and (not db.query(Users).filter_by(login=login).first().verify_password(password)):
-        raise HTTPException(status_code=401, detail="Incorrect password or login")
+async def create_session(response: Response, login: str = Body(), password: str = Body(),
+                         db: SessionLocal = Depends(get_db)):
+    if db.query(Users).filter_by(login=login).first() is None:
+        raise HTTPException(status_code=401, detail="User does not exist")
+    if not db.query(Users).filter_by(login=login).first().verify_password(password):
+        raise HTTPException(status_code=401, detail="Incorrect password")
     session = uuid4()
     data = SessionData(username=login)
 
@@ -131,17 +141,18 @@ async def del_session(response: Response, session_id: UUID = Depends(cookie)):
     return "deleted session"
 
 
-
-@app.post("/users")
+@app.post("/register")
 async def add_user(
-    name: str = Body(),
-    surname: str = Body(),
-    login: str = Body(),
-    password: str = Body(),
-    phone_nr: str = Body(),
-    db: SessionLocal = Depends(get_db)
+        name: str = Body(),
+        surname: str = Body(),
+        login: str = Body(),
+        password: str = Body(),
+        phone_nr: str = Body(),
+        db: SessionLocal = Depends(get_db)
 ):
-    user_id = max([row[0] for row in db.query(Users.id).all()] + [-1]) + 1
+    user_id = max([row[0] for row in db.query(Users.id).all()] + [-1]) + 1  # TODO switch to autoincrement in future pls
+    if db.query(Users).filter_by(login=login).first() is not None:
+        raise HTTPException(status_code=409, detail="Username already exists")
     user = Users(
         id=user_id,
         name=name,
@@ -152,10 +163,10 @@ async def add_user(
     user.set_password(password)
     db.add(user)
     db.commit()
-    return user_id
+    return f"user {login} created"
 
 
-@app.delete("/users")
+@app.delete("/users", dependencies=[Depends(cookie)])
 async def delete_user(session_data: SessionData = Depends(verifier), db: SessionLocal = Depends(get_db)):
     user = db.query(Users).filter(Users.login == session_data.username).first()
     if not user:
@@ -164,28 +175,46 @@ async def delete_user(session_data: SessionData = Depends(verifier), db: Session
     db.commit()
 
 
-@app.get("/offers")
-async def read_offers(db: SessionLocal = Depends(get_db)):
-    offers = db.query(Offers).all()
+@app.get("/offers", dependencies=[Depends(cookie)])
+async def read_offers(db: SessionLocal = Depends(get_db), session_data: SessionData = Depends(verifier)):
+    offers = db.execute(
+        select(Offers.c["latitude", "longitude", "price"], Dishes.c["name", "description", "price", "how_many_days_before_expiration"], Users.c["name", "surname"]).join(Dishes).join(Offers.seller_id).where(Offers.state == 0)
+    )
     return offers
 
 
-@app.post("/offers")
+@app.post("/offers", dependencies=[Depends(cookie)])
 async def add_offer(
-    latitude: float = Body(),
-    longitude: float = Body(),
-    dish_id: int = Body(),
-    seller_id: int = Body(),
-    db: SessionLocal = Depends(get_db)
+        latitude: float = Body(),
+        longitude: float = Body(),
+        dish_name: str = Body(),
+        description: str = Body(),
+        price: int = Body(),  # price in grosze
+        how_many_days_before_expiration: float = Body(),
+        db: SessionLocal = Depends(get_db),
+        session_data: SessionData = Depends(verifier)
 ):
     offer_id = max([row[0] for row in db.query(Offers.id).all()] + [-1]) + 1
+    user_id = db.query(Users).filter_by(login=session_data.username).first().id
+
+    dish_id = max([row[0] for row in db.query(Dishes.id).all()] + [-1]) + 1
+    dish = Dishes(
+        id=dish_id,
+        name=dish_name,
+        price=price,
+        description=description,
+        how_many_days_before_expiration=how_many_days_before_expiration,
+        author_id=user_id
+    )
+    db.add(dish)
+
     offer = Offers(
         id=offer_id,
         dish_id=dish_id,
         latitude=latitude,
-        longitude = longitude,
+        longitude=longitude,
         state=OfferState.OPEN,
-        seller_id=seller_id,
+        seller_id=user_id,
         creation_date=current_timestamp()
     )
     db.add(offer)
@@ -214,21 +243,24 @@ async def read_dishes(db: SessionLocal = Depends(get_db)):
     return dishes
 
 
-@app.post("/dishes")
+@app.post("/dishes", dependencies=[Depends(cookie)])
 async def add_dish(
-    name: str = Body(),
-    description: str = Body(),
-    price: int = Body(),
-    how_many_days_before_expiration: float = Body(),
-    db: SessionLocal = Depends(get_db)
+        name: str = Body(),
+        description: str = Body(),
+        price: int = Body(),
+        how_many_days_before_expiration: float = Body(),
+        db: SessionLocal = Depends(get_db),
+        session_data: SessionData = Depends(verifier)
 ):
+    user_id = db.query(Users).filter_by(login=session_data.username).first().id
     dish_id = max([row[0] for row in db.query(Dishes.id).all()] + [-1]) + 1
     dish = Dishes(
         id=dish_id,
         name=name,
         price=price,
         description=description,
-        how_many_days_before_expiration=how_many_days_before_expiration
+        how_many_days_before_expiration=how_many_days_before_expiration,
+        author_id=user_id
     )
     db.add(dish)
     db.commit()
@@ -250,7 +282,6 @@ async def delete_dish(dish_id: int, db: SessionLocal = Depends(get_db)):
     db.commit()
 
 
-
 @app.get("/dishtags")
 async def read_dishtags(db: SessionLocal = Depends(get_db)):
     dishtags = db.query(DishTags).all()
@@ -259,9 +290,9 @@ async def read_dishtags(db: SessionLocal = Depends(get_db)):
 
 @app.post("/dishtags")
 async def add_dishtag(
-    dish_id: int = Body(),
-    tag_id: int = Body(),
-    db: SessionLocal = Depends(get_db)
+        dish_id: int = Body(),
+        tag_id: int = Body(),
+        db: SessionLocal = Depends(get_db)
 ):
     dishtag_id = max([row[0] for row in db.query(DishTags.id).all()] + [-1]) + 1
     dishtag = Dishes(
@@ -288,6 +319,7 @@ async def delete_dishtag(dishtag_id: int, db: SessionLocal = Depends(get_db)):
     db.delete(dishtag)
     db.commit()
 
+
 @app.get("/get_tags_map")
 async def get_tags_map():
     return {
@@ -296,7 +328,8 @@ async def get_tags_map():
         2: "sugarFree",
         3: "shouldBeWarm",
         4: "spicy"
-        }
+    }
+
 
 @app.post("/add_tags_to_dish")
 async def add_tags_to_dish(dish_id: int, list_of_tag_id: list[int]):
@@ -306,15 +339,15 @@ async def add_tags_to_dish(dish_id: int, list_of_tag_id: list[int]):
 
 @app.post("/add_full_offer")
 async def add_offer_full(
-    latitude: float = Body(),
-    longitude: float = Body(),
-    seller_id: int = Body(), # get user id from session
-    name: str = Body(),
-    description: str = Body(),
-    price: int = Body(),
-    how_many_days_before_expiration: float = Body(),
-    list_of_tag_id: list[int] = Body(),
-    db: SessionLocal = Depends(get_db)
+        latitude: float = Body(),
+        longitude: float = Body(),
+        seller_id: int = Body(),  # get user id from session
+        name: str = Body(),
+        description: str = Body(),
+        price: int = Body(),
+        how_many_days_before_expiration: float = Body(),
+        list_of_tag_id: list[int] = Body(),
+        db: SessionLocal = Depends(get_db)
 ):
     dish_id = add_dish(name, description, price, how_many_days_before_expiration)
     add_tags_to_dish(dish_id, list_of_tag_id)

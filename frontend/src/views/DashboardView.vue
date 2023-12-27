1a8c7c6bc338dad67dd7485ca1810c98276714e3
Adam Jeliński <adas.jelinski@gmail.com>
2023-12-27 03:40:49 +0100

    feat: add new home page and dashboard REF-26

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,7 +1,65 @@
-<template>
-    <HelloWorld />
-</template>
+<script setup></script>
 
-<script setup>
-import HelloWorld from '@/components/HelloWorld.vue'
-</script>
+<template>
+    <v-container class="fill-height">
+        <v-responsive class="align-center text-center fill-height">
+            <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+
+            <h1 class="text-h2 font-weight-bold">ReFood</h1>
+
+            <div class="py-14" />
+
+            <v-row class="d-flex align-center justify-center">
+                <v-col cols="auto">
+                    <v-btn
+                        min-width="164"
+                        variant="text"
+                        to="/myOffers"
+                    >
+                        <v-icon
+                            icon="mdi-food"
+                            size="large"
+                            start
+                        />
+
+                        My offers
+                    </v-btn>
+                </v-col>
+
+                <v-col cols="auto">
+                    <v-btn
+                        color="primary"
+                        min-width="228"
+                        size="x-large"
+                        variant="flat"
+                        to="/search"
+                    >
+                        <v-icon
+                            icon="mdi-magnify"
+                            size="large"
+                            start
+                        />
+
+                        Search offers
+                    </v-btn>
+                </v-col>
+
+                <v-col cols="auto">
+                    <v-btn
+                        min-width="164"
+                        variant="text"
+                        to="/addOffer"
+                    >
+                        <v-icon
+                            icon="mdi-plus-box"
+                            size="large"
+                            start
+                        />
+
+                        Add offer
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-responsive>
+    </v-container>
+</template>

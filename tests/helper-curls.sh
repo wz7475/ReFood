a98445459dae500c4f0a5138a6f33099538d4574
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-01-01 13:13:22 +0100

    refactor: use single process for all es queues REF-44

--- tests/helper-curls.sh
+++ tests/helper-curls.sh
@@ -11,4 +11,3 @@ http://localhost:8080/offers --cookie "cookie.txt"
 
 curl http://localhost:8080/test-es-query/example --cookie "cookie.txt"
 
-curl http://localhost:8080/test-es-query/example --cookie "cookie.txt"

e787852380ab83de34855bc358a548a562c4af4b
rszczep2 <01169247@pw.edu.pl>
2024-01-09 08:42:50 +0100

    Tests that dont need sessions

--- tests/tests/test_endpoints.py
+++ tests/tests/test_endpoints.py
@@ -1,18 +1,33 @@
 import requests
+from fastapi import HTTPException
+import json
 
 from cfg_tests import SESSION_COOKIE_FIELD, ADDRESS
-from tools import register, login
+from tools import register, login, add_offers, get_offers
 
 
-# ADDRESS = 'http://api:80'
-def test_dummy_root():
-    register_response = register()
-    assert register_response.status_code == 200
-    login_response, cookie = login()
-    assert login_response.status_code == 200
-    response = requests.get(f"{ADDRESS}/", cookies={SESSION_COOKIE_FIELD: cookie})
-    assert response.status_code == 200
-    assert response.json() == 'ReFood'
+def test_register_ok():
+    register_response = register('test14', 'test3')
+    assert json.loads(register_response.text) == 'user test14 created'
 
-if __name__ == '__main__':
-    test_dummy_root()
+def test_register_login_used():
+    register_response = register('test4', 'test3')
+    assert json.loads(register_response.text).get('detail') == 'Username already exists'
+
+def test_login_ok():
+    login_response, cookie = login('test12', 'test3')
+    assert json.loads(login_response.text) == 'created session for test12'
+
+def test_login_bad_login():
+    try:
+        login_response, cookie = login('bad_login', 'test3555')
+    except HTTPException as e:
+        assert HTTPException.status_code == 401
+        assert HTTPException.detail == 'User does not exist'
+
+def test_login_bad_password():
+    try:
+        login_response, cookie = login('test4', 'test3555')
+    except HTTPException as e:
+        assert HTTPException.status_code == 401
+        assert HTTPException.detail == "Incorrect password"
--- tests/tests/tools.py
+++ tests/tests/tools.py
@@ -1,28 +1,44 @@
 import random
 
 import requests
-
+from uuid import uuid4
 from cfg_tests import ADDRESS, get_uuid_string, LOGIN, PASSWORD, SESSION_COOKIE_FIELD
-
 
 def get_random_phone_number()->str:
     return str(random.randint(100000000, 999999999))
 
 
-def register():
+def register(login, password):
     response = requests.post(ADDRESS + '/register', json={
         'name': get_uuid_string(),
         'surname': get_uuid_string(),
-        'login': LOGIN,
-        'password': PASSWORD,
+        'login': login,
+        'password': password,
         'phone_nr': get_random_phone_number()
     })
     return response
 
-def login():
+def login(login, password):
     response = requests.post(ADDRESS + '/login', json={
-        'login': LOGIN,
-        'password': PASSWORD
+        'login': login,
+        'password': password
     })
     session_cookie = response.cookies.get(SESSION_COOKIE_FIELD)
     return response, session_cookie
+
+def add_offers():
+    _, cookie = login('test12', 'test3')
+    response = requests.post(ADDRESS + '/offers', json={
+        "latitude": 1,
+        "longitude": 1,
+        "dish_name": "kolejna taggi",
+        "description": "to nmoże  są banany",
+        "price": 1122,
+        "how_many_days_before_expiration": 1,
+        "tags": [0, 2]
+    })
+    return response
+
+def get_offers():
+    _, cookie = login('test12', 'test3')
+    return requests.get(ADDRESS + '/offers')
